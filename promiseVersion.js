1241796e0a099e6638b4d8ed695f8a52c97500ce
Chris Lambert <60009194+chrislambert-ky@users.noreply.github.com>
2025-06-28 16:21:34 -0400

    Completed Promise Version

--- promiseVersion.js
+++ promiseVersion.js
@@ -1,1 +1,44 @@
 // Summer Chores Promise Version
+
+function mowYard(name)
+{
+    return new Promise((resolve) =>
+    {
+    setTimeout(() =>
+        {console.log(`${name} mowed the yard.`);resolve();}, 1000);
+    });
+}
+
+function weedEat(name)
+{
+    return new Promise((resolve, reject) =>
+    {
+    setTimeout(() =>
+        {
+            if (Math.random() < 0.25)
+            {console.log(`${name} fell asleep and failed to get things done.`);reject();}
+            else
+            {console.log(`${name} finished using the weed eater.`);resolve();}
+        }, 1000);
+    });
+}
+
+function trimHedges(name)
+{
+    return new Promise((resolve) =>
+    {
+    setTimeout(() =>
+        {console.log(`${name} finished trimming the hedges.`);resolve();}, 1000);
+    });
+}
+
+function doSummerChores(name)
+{
+    mowYard(name)
+    .then(() => weedEat(name))
+    .then(() => trimHedges(name))
+    .then(() => {console.log(`${name} finished all their chores!`);})
+    .catch(() => {console.log(`${name} did not finish all their chores.`);});
+}
+
+doSummerChores("Chris");
